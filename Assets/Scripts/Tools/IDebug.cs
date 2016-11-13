444580b94137a7769f280cca8998ed0faa6264ce
Artur Salyakhov <artykod@gmail.com>
2016-11-13 16:26:07 +0300

    правка структуры скриптов.

--- Assets/Scripts/Tools/IDebug.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public interface IDebug
-{
-	void Log(string format, params object[] args);
-	void LogWarning(string format, params object[] args);
-	void LogError(string format, params object[] args);
-	void LogException(Exception e);
-}
